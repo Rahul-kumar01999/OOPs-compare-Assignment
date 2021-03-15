3c095aa816894abb2b3b0fcdc745e9e0fc6d8df6
Rahul-kumar01999 <rahul.kumar@knoldus.com>
2021-03-15 22:13:32 +0530

    added scoverage and scalastyle plugins

---
+++ Compare-Person/src/test/scala/com/knoldus/PersonTest.scala
@@ -0,0 +1,52 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//scalastyle:off magic.number
+package com.knoldus
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class PersonTest extends AnyFlatSpec {
+
+//cases for checking whether PersonOne name length and age is <,> then PersonTwo
+  "person" should "return True as age of PersonOne < PersonTwo " in{
+    val personOne = new Person("Test", 24)
+    val personTwo = new Person("Test",25)
+    val result = personOne < personTwo
+    assert(result)
+  }
+
+  "person" should "return False as age of PersonOne > PersonTwo " in{
+    val personOne = new Person("Test", 25)
+    val personTwo = new Person("Test",24)
+    val result = personOne > personTwo
+    assert(result)
+  }
+
+  "person" should "return True as Length of name of PersonOne < PersonTwo " in{
+    val personOne = new Person("Test", 24)
+    val personTwo = new Person("TestAgain",24)
+    val result = personOne < personTwo
+    assert(result)
+  }
+
+  "person" should "return False as Length of name of PersonOne > PersonTwo " in{
+    val personOne = new Person("TestAgain", 25)
+    val personTwo = new Person("Test",25)
+    val result = personOne > personTwo
+    assert(result)
+  }
+}
